cdb67de27d28ed59e3a4322ab8e2c785821edfd6
William Pinto <william.pinto@william.pinto>
2021-05-03 16:30:53 -0300

    STARTING COURSE_MODULE 1: Introduction to Datomic

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-datomic
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_datomic/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-datomic.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-datomic.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
